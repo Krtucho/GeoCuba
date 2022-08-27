a8d2f6e9982e6dbcf6a7a2f64263f8e899d9453b
Krtucho <krtucho98@gmail.com>
2022-08-27 09:55:10 -0400

    Deploying to Heroku

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [{
         children: [{
                 path: '',
                 component: () =>
-                    import ('pages/IndexPage.vue')
+                    import ('pages/Login.vue')
             },
             {
                 path: 'login',
