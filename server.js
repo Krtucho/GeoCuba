3347d4f7a369e3e581718da3bfaf27097f9e296e
Krtucho <krtucho98@gmail.com>
2022-08-27 08:52:00 -0400

    Deploying to Heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  path = require('path'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+// const
+//   express = require('express'),
+//   serveStatic = require('serve-static'),
+//   path = require('path'),
+//   history = require('connect-history-api-fallback'),
+//   port = process.env.PORT || 5000
 
 //const app = express()
 
@@ -11,11 +11,27 @@ const
 //app.use(serveStatic(__dirname + '/dist/spa'))
 //app.listen(port)
 
+// const app = express()
+// app.use(history())
+// app.use(serveStatic(__dirname + '/dist/spa'))
+// app.get('/*', function(req,res) {
+//     //res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
+//     res.sendFile(path.join(__dirname, 'dist/quasar-app', 'index.html'));
+// });
+// app.listen(port);
+
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
+
 const app = express()
+
+// attackers can use this header to detect apps running Express
+// and then launch specifically-targeted attacks
+app.disable('x-powered-by')
+
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
-app.get('/*', function(req,res) {
-    //res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
-    res.sendFile(path.join(__dirname, 'dist/quasar-app', 'index.html'));
-});
-app.listen(port);
+app.listen(port)
