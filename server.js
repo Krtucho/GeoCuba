3b8cc22e3abdbe4e36920a6479a90c1d55377a43
Krtucho <krtucho98@gmail.com>
2022-08-27 08:35:40 -0400

    Deploying to Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const
   history = require('connect-history-api-fallback'),
   port = process.env.PORT || 5000
 
-const app = express()
+//const app = express()
 
 //app.use(history())
 //app.use(serveStatic(__dirname + '/dist/spa'))
